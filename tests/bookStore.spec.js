64721912d48e130295422dce551d73e19845a8dd
darya-mac <d.kashenko@lipt-soft.ru>
2022-08-17 15:38:10 +0300

    new commit

--- tests/bookStore.spec.js
+++ tests/bookStore.spec.js
@@ -8,7 +8,7 @@ describe ('Books store swagger', () => {
 
       const res = await books.addListOfBooks({userId: '4c9e5b2b-bdac-4710-8285-23aa5527b7ea', ibsn: '9781449325862'})
       expect(res.status).toEqual(201)
-  
+   
         
 
     })
