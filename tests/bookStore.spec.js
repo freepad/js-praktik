b512062a243ee91ba6aab72636146fd10ed4585e
darya-mac <d.kashenko@lipt-soft.ru>
2022-08-18 12:25:38 +0300

    udate token

--- tests/bookStore.spec.js
+++ tests/bookStore.spec.js
@@ -8,5 +8,4 @@ describe ('Books store swagger', () => {
       const res = await books.addListOfBooks({userId: '4c9e5b2b-bdac-4710-8285-23aa5527b7ea', ibsn: '9781449325862'})
       expect(res.status).toEqual(201)
     })
-
 })
